16382cbb0bc35cf614239126ed1544414959d99e
Nuray Şentürk <83533779+nry54@users.noreply.github.com>
2025-10-27 21:52:55 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        {/* <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav> */}
+      </header>
+      <main>
+        <section>
+          <h1>Welcome to my portfolio</h1>
+          <p>
+            I'm a computer engineer with a passion for building web applications
+          </p>
+          <button>Contact me</button>
+        </section>
+      </main>
+      <footer>
+        <p>Copyright 2025 Nuray Şentürk</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;

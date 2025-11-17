86aefbe429a5724cb2f677461a9348b05192e420
Nuray Şentürk <83533779+nry54@users.noreply.github.com>
2025-11-17 23:58:26 +0300

    Added section styling and updated about page layout

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,8 +1,16 @@
 const About = () => {
   return (
     <>
-      <h2>About</h2>
-      <p>This is the about page</p>
+      <section className="section">
+        <h2 className="sectionTitle">About</h2>
+        <p>
+          I am an experienced full stack developer with years of expertise in
+          Vue.js and PHP. I have developed software for artificial intelligence,
+          medicine, Cloud ERP, and educational systems. I am currently deepening
+          my knowledge in front-end development and am in the process of
+          learning React. I prioritize clean, responsive, and UI/UX design.
+        </p>
+      </section>
     </>
   );
 };
